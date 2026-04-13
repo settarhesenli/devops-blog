48054162384947f81386d8dbfe5f2e8753391b0e
Ubuntu <ubuntu@ip-172-31-44-116.ec2.internal>
2026-04-13 16:49:35 +0000

    some fixes

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = 3000;
 
 app.get("/", (req, res) => {
   res.send(`
-    <h1>🚀 Settar's DevOps Blog</h1>
+    <h1>🚀 Settar's DevOps Blog. I did it. Yeah</h1>
     <p>CI/CD yoldadır...</p>
     <p>Version: 2.0</p>
   `);
